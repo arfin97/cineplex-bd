077d3078c9ea800fead89690c4ab24825b9cb28c
arfin97 <arfin97@gmail.com>
2023-08-23 01:26:17 +0600

    Refactored movie controller and added swagger info

--- src/main/java/com/cineplexbd/cineplex/controller/MovieController.java
+++ src/main/java/com/cineplexbd/cineplex/controller/MovieController.java
@@ -2,7 +2,13 @@ package com.cineplexbd.cineplex.controller;
 
 import com.cineplexbd.cineplex.domain.MovieRequest;
 import com.cineplexbd.cineplex.domain.MovieResponse;
+import com.cineplexbd.cineplex.entities.Movie;
 import com.cineplexbd.cineplex.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController("/api")
 public class MovieController {
 
     private final MovieService movieService;
@@ -19,21 +25,30 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/api/movies/{id}")
+    @Operation(summary = "Get a movie by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the movie",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Movie not found",
+                    content = @Content) })
+    @GetMapping("/movies/{id}")
     public ResponseEntity<MovieResponse> getMovieById(@PathVariable Long id){
         MovieResponse movieResponse = movieService.getMovieById(id);
         return ResponseEntity.ok(movieResponse);
     }
 
-    @PostMapping("/api/movies")
-    public ResponseEntity<MovieResponse> createMovie(@Valid @RequestBody MovieRequest movieRequest){
-        MovieResponse savedMovie = movieService.createMovie(movieRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedMovie);
-    }
-
-    @GetMapping("/api/movies")
+    @GetMapping("/movies")
     public ResponseEntity<List<MovieResponse>> getAllMovies(){
         List<MovieResponse> movieResponses = movieService.getAllMovies();
         return ResponseEntity.ok(movieResponses);
+    }
+
+    @PostMapping("/movies")
+    public ResponseEntity<MovieResponse> createMovie(@Valid @RequestBody MovieRequest movieRequest){
+        MovieResponse savedMovie = movieService.createMovie(movieRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedMovie);
     }
 }
