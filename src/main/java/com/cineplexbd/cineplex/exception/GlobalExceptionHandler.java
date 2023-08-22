e3fe4b907fa49187caffc0abac85215268b47af7
arfin97 <arfin97@gmail.com>
2023-08-23 01:25:56 +0600

    Added OpenApi 3 and Swagger UI for the APIs

--- src/main/java/com/cineplexbd/cineplex/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cineplexbd/cineplex/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
@@ -16,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
